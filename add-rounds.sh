5f96a95763cb8786cf43d74084eb6a41b6329b87
Slava Andreyev <slava9295@gmail.com>
2021-05-29 17:19:49 -0400

    added a new round

--- add-rounds.sh
+++ add-rounds.sh
@@ -6,10 +6,8 @@ data_dir=CombinedScorecards
 
 cp empty.json standings.json
 for round in "$data_dir"/20*.csv; do
-    if (( $(wc -l < "$round") > 3 )); then
-        python add-round.py standings.json "$round" standings-new.json
-        mv standings-new.json standings.json
-    fi
+    python add-round.py standings.json "$round" standings-new.json
+    mv standings-new.json standings.json
 done
 
 python print-ratings.py standings.json --markdown > standings.md
